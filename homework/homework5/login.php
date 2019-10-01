95988e1243b39c6b28136eb013db2fa58c4a5d01
bjpettigrew <bjpettigrew@gmail.com>
2019-10-01 06:03:37 +0000

    2.3

--- homework/homework5/login.php
+++ homework/homework5/login.php
@@ -5,7 +5,49 @@
 <?php include('includes/header.php'); ?>
 <?php include('includes/style.css');?>
 
+<style>
 
+    ul {
+        margin-left: 5px;
+        }
+    li {
+        float: left;
+        list-style-type: none;
+        background-color: #fd6a02;
+        border: 2px solid black;
+        padding: 10px 5%;
+        margin-top: 1%;
+        margin-left: 2%;
+        }
+
+    li a {
+        display: block;
+        color: black;
+        text-align: center;
+        text-decoration: none;
+        }
+    li a:hover {
+        background-color: #F79682;
+        }
+
+    h1 {
+        text-align: center;
+        padding: 2%;
+        }
+    body {
+        background-color: #fd6a02;
+        }
+    .container {
+        border: ridge 10px grey;
+        padding: 3%;
+        margin-top: 2%;
+        font-weight: bold;
+        }
+    button {
+        background-color: #fd6a02;
+        }
+
+</style>
 </head>
 <!-- html login page !-->
 <h1>Login Page</h1>
