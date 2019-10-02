6272361ca2e5d4348bb814d1729f1993aa581179
bjpettigrew <bjpettigrew@gmail.com>
2019-10-02 01:58:35 +0000

    make new folders for homework5

---
+++ homework/homework5/index.php
@@ -0,0 +1,57 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Homework 5 Practice - index</title>
+        <link rel="stylesheet" type="text/css" href="includes/styles.css">
+        <link href="https://fonts.googleapis.com/css?family=Poppins|Prata&display=swap" rel="stylesheet">
+    </head>
+
+    <?php //This includes the HEADER and the header navigation
+        include ("includes/header.php");
+    ?>
+
+        <img src="https://i.imgur.com/ahmGO8L.jpg" alt="picture of various sushi">
+
+        <main>
+            <section class="index_sections">
+                <h2>Sushi? Success!</h2>
+                    <section id="text_intro">
+                        <p>Welcome to Sushi Success!</p>
+                        <p>Located right by the beaches of Fake City, CA, we offer only the freshest seafood, selected daily by our chefs. From sashimi to nigiri to handrolls, we have it all! Our Happy Hour specials are paralleled by none. We offer an extensive list that includes our famous handcrafted lychee martinis, a seasonal collection of warm (or cold) sake, and more imported beers than you can shake a stick at. </p>
+                        <p>Come enjoy a bento box and relax on our beachfront deck, with a front row seat to the Pacific Ocean, the music of the waves, and a sunset waiting just for you!</p><br>
+
+                        <h3>Sushi Success Rewards</h3>
+                        <p>Ever wanted to join a sushi club? Now you can! Sign up and you'll be able to rack up reward points that can be redeemed for food, drinks, and swag during your birthday or anniversary.</p><br>
+                        <center><p><a id="sign_up_link" href="register.php">Sign up today!</a></p></center>
+                    </section>
+                </section>
+
+            <section class="index_sections">
+                <section id="hours_section"></section>
+                    <h3>Hours</h3>
+                    <p>Sunday - 11AM-10PM</p>
+                    <p>Monday - Closed</p>
+                    <p>Tuesday - 11AM-9PM</p>
+                    <p>Wednesday - 11AM-9PM</p>
+                    <p>Thursday - 11AM-9PM</p>
+                    <p>Friday - 11AM-10PM</p>
+                    <p>Saturday - 11AM-10PM</p>
+
+                    <br>
+
+                    <p><img src="https://i.imgur.com/MuYcaMV.jpg" alt="picture of an evening with a restaurant table and wine glasses"></p>
+                </section>
+            </section>
+
+        </main>
+
+    <?php  //This includes the FOOTER
+        include ("includes/footer.php");
+    ?>
+
+
+
+
+
+
+
---
+++ homework/homework5/login.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Homework 5 Practice - index</title>
+        <link rel="stylesheet" type="text/css" href="includes/styles.css">
+        <link href="https://fonts.googleapis.com/css?family=Poppins|Prata&display=swap" rel="stylesheet">
+    </head>
+
+    <?php //This includes the HEADER and the header navigation
+        include ("includes/header.php");
+    ?>
+
+    <?php  //This includes the LOGIN
+        include ("includes/inc_login.php");
+    ?>
+
+<section id="footer_login">
+    <?php  //This includes the FOOTER
+        include ("includes/footer.php");
+    ?>
+</section>
+
